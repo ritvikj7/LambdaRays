d494925b2927ac752dd1db7014c8e7848df82ec6
ritvik <ritvikj@students.cs.ubc.ca>
2024-10-22 22:37:49 -0700

    CPSC-312: changing the order or actual and expected in our test cases to follow the convention

--- haskell/test/Spec.hs
+++ haskell/test/Spec.hs
@@ -11,23 +11,23 @@ v2 = Vec3f 4 5 6
 main :: IO ()
 main = defaultMain $ testGroup "Vec3 Tests" 
   [ testCase "Addition works" $ do
-      (v1 + v2) @?= Vec3f 5 7 9
+      Vec3f 5 7 9 @?= (v1 + v2)
   , testCase "Subtraction works" $ do
-      (v1 - v2) @?= Vec3f (-3) (-3) (-3)
+      Vec3f (-3) (-3) (-3) @?= (v1 - v2)
   , testCase "Negation works" $ do
-      negate v1 @?= Vec3f (-1) (-2) (-3)
+      Vec3f (-1) (-2) (-3) @?= negate v1
   , testCase "Scalar multiplication (left) works" $ do
-      (2.0 *^ v1) @?= Vec3f 2.0 4.0 6.0
+      Vec3f 2.0 4.0 6.0 @?= (2.0 *^ v1)
   , testCase "Scalar multiplication (right) works" $ do
-      (v1 *. 3.0) @?= Vec3f 3.0 6.0 9.0
+      Vec3f 3.0 6.0 9.0 @?= (v1 *. 3.0)
   , testCase "Dot product works" $ do
-      (v1 `dot` v2) @?= 32.0
+      32.0 @?= (v1 `dot` v2)
   , testCase "Cross product works" $ do
-    (v1 `cross` v2) @?= Vec3f (-3.0) 6.0 (-3.0)
+      Vec3f (-3.0) 6.0 (-3.0) @?= (v1 `cross` v2)
   , testCase "Vector length works" $ do
-      vecLength v1 @?= sqrt 14.0
+      sqrt 14.0 @?= vecLength v1
   , testCase "Vector distance works" $ do
-      vecDistance v1 v2 @?= sqrt 32.0
+      sqrt 32.0 @?= vecDistance v1 v2
   , testCase "Vector normalization works" $ do
-      vecNormalize v1 @?= Vec3f (1.0 / sqrt 14.0) (2.0 / sqrt 14.0) (3.0 / sqrt 14.0)
+      Vec3f (1.0 / sqrt 14.0) (2.0 / sqrt 14.0) (3.0 / sqrt 14.0) @?= vecNormalize v1
   ]
